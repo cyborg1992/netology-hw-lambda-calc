3e36b7c95ba0c103f0c9a6e1a9db4999920030d8
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-04-19 22:31:51 +0300

    add .gitignore

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,14 +2,14 @@ import java.util.function.*;
 
 public class Calculator {
     public static Supplier<Calculator> instance = Calculator::new;
-    BinaryOperator<Integer> plus = (x, y) -> x + y;
-    BinaryOperator<Integer> minus = (x, y) -> x - y;
-    BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? 0 : x / y; //ошибка возникает при делении на ноль
+    public BinaryOperator<Integer> plus = (x, y) -> x + y;
+    public BinaryOperator<Integer> minus = (x, y) -> x - y;
+    public BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    public BinaryOperator<Integer> divide = (x, y) -> y == 0 ? 0 : x / y; //ошибка возникает при делении на ноль
     //как вариант решения необходимо проверить "y" и если он равен нулю то вернуть нужное значение (я вернул 0)
 
-    UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-    Predicate<Integer> isPositive = x -> x > 0;
-    Consumer<Integer> println = System.out::println;
+    public UnaryOperator<Integer> pow = x -> x * x;
+    public UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    public Predicate<Integer> isPositive = x -> x > 0;
+    public Consumer<Integer> println = System.out::println;
 }
