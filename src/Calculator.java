31e58e9d318f7b8b581649ea3ea9be2ea6d3bd9c
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-04-19 22:30:19 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.function.*;
+
+public class Calculator {
+    public static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> y == 0 ? 0 : x / y; //ошибка возникает при делении на ноль
+    //как вариант решения необходимо проверить "y" и если он равен нулю то вернуть нужное значение (я вернул 0)
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
